7616b61eabac2498959c16e798d170dfaa0202c3
shuzipper <shuzipper@gmail.com>
2024-07-26 00:09:04 +0800

    合并远程分支解决冲突

--- mqtt/CraneData.py
+++ mqtt/CraneData.py
@@ -1,12 +1,15 @@
+import struct
+import json
+import time
 
-class CraneData:
+class CraneDataHandler:
     def __init__(self) -> None:
         self.lock_state = 0
         self.unlock_state = 0
         self.landed_state = 0
-        self._20ft = 0
-        self._40ft = 0
-        self._45ft = 0
+        self.sprd20ft = 0
+        self.sprd40ft = 0
+        self.sprd45ft = 0
         self.singleMode = 0
         self.twinMode = 0
         self.trolleyLimit = 0
@@ -24,4 +27,61 @@ class CraneData:
         self.loadSpeedX = 0
         self.skewAngle = 0
         self.twinGap = 0
+    
+    def parse_data(self, raw_data):
 
+        word1_low = struct.unpack("b",raw_data[0:1])[0]
+        word1_high = struct.unpack("b",raw_data[1:2])[0]
+        self.lock_state = (word1_low >> 0) & 1
+        self.unlock_state = (word1_low >> 1) & 1
+        self.landed_state = (word1_low >> 2) & 1
+        self.sprd20ft = (word1_low >> 3) & 1
+        self.sprd40ft = (word1_low >> 4) & 1
+        self.sprd45ft = (word1_low >> 5) & 1
+        self.singleMode = (word1_low >> 6) & 1
+        self.twinMode = (word1_low >> 7) & 1
+        self.trolleyLimit = (word1_high >> 0) & 1
+        self.hoistLimit = (word1_high >> 1) & 1
+        self.flipperSlDown = (word1_high >> 3) & 1
+        self.flipperLlDown = (word1_high >> 4) & 1
+        self.flipperLrDown = (word1_high >> 5) & 1
+        self.flipperSrDown = (word1_high >> 6) & 1
+        self.test_delay_fb = (word1_high >> 7) & 1
+
+        self.trolleyPos = round(float(struct.unpack("i",raw_data[2:6])[0])/1000,3)
+        self.hoistPos = round(float(struct.unpack("i",raw_data[6:10])[0])/1000,3)
+        self.loadPosX = round(float(struct.unpack("i",raw_data[10:14])[0])/1000,3)
+        self.trolleySpeed = round(float(struct.unpack("h",raw_data[14:16])[0])/16384*4.0,3)
+        self.hoistSpeed = round(float(struct.unpack("h",raw_data[16:18])[0])/16384*3.0,3)
+        self.loadSpeedX = round(float(struct.unpack("h",raw_data[18:20])[0])/1000,3)
+        self.skewAngle = round(float(struct.unpack("h",raw_data[20:22])[0])/1000,3)
+        self.twinGap = struct.unpack("h",raw_data[22:24])[0]
+
+     # 定义一个函数，将解析得到的数据转换为json格式并返回
+    def data_to_json(self):
+        return json.dumps({
+            "timestamp": time.strftime("%Y-%m-%d:%H:%M:%S"),
+            "lock_state": self.lock_state,
+            "unlock_state": self.unlock_state,
+            "landed_state": self.landed_state,
+            "sprd20ft": self.sprd20ft,
+            "sprd40ft": self.sprd40ft,
+            "sprd45ft": self.sprd45ft,
+            "singleMode": self.singleMode,
+            "twinMode": self.twinMode,
+            "trolleyLimit": self.trolleyLimit,
+            "hoistLimit": self.hoistLimit,
+            "flipperSlDown": self.flipperSlDown,
+            "flipperLlDown": self.flipperLlDown,
+            "flipperLrDown": self.flipperLrDown,
+            "flipperSrDown": self.flipperSrDown,
+            "test_delay_fb": self.test_delay_fb,
+            "trolleyPos": self.trolleyPos,
+            "hoistPos": self.hoistPos,
+            "loadPosX": self.loadPosX,
+            "trolleySpeed": self.trolleySpeed,
+            "hoistSpeed": self.hoistSpeed,
+            "loadSpeedX": self.loadSpeedX,
+            "skewAngle": self.skewAngle,
+            "twinGap": self.twinGap
+        },indent = 4)
--- mqtt/mqtt_client.py
+++ mqtt/mqtt_client.py
@@ -1,10 +1,9 @@
 import asyncio
 import paho.mqtt.client as mqtt
-import struct
 
-from CraneData import CraneData
+from CraneData import CraneDataHandler
 
-data_frame = CraneData()
+data_frame = CraneDataHandler()
 
 # Callback when the client connects to the broker
 def on_connect(client, userdata, flags, rc):
@@ -14,44 +13,9 @@ def on_connect(client, userdata, flags, rc):
 # Callback when a message is received
 def on_message(client, userdata, msg):
     # 解析数据
-    parsed_data =  parse_data(msg.payload)
-    print(f"Receive data : {parsed_data}")
+    data_frame.parse_data(msg.payload)
 
-    # print(f"Topic: {msg.topic}, Message: {list_data}")
-
-def parse_data(raw_data):
-    data = {}
-    word1_low = struct.unpack("b",raw_data[0:1])[0]
-    word1_high = struct.unpack("b",raw_data[1:2])[0]
-
-    data_frame.lock_state = (word1_low >> 0) & 1
-    #data["lock_state"] = (word1_low >> 0) & 1
-    data["unlock_state"] = (word1_low >> 1) & 1
-    data["landed_state"] = (word1_low >> 2) & 1
-    data["20ft"] = (word1_low >> 3) & 1
-    data["40ft"] = (word1_low >> 4) & 1
-    data["45ft"] = (word1_low >> 5) & 1
-    data["singleMode"] = (word1_low >> 6) & 1
-    data["twinMode"] = (word1_low >> 7) & 1
-
-    data["trolleyLimit"] = (word1_high >> 0) & 1  
-    data["hoistLimit"] = (word1_high >> 1) & 1 
-    data["flipperSlDown"] = (word1_high >> 3) & 1
-    data["flipperLlDown"] = (word1_high >> 3) & 1
-    data["flipperLrDown"] = (word1_high >> 4) & 1
-    data["flipperSrDown"] = (word1_high >> 5) & 1
-    data["test_delay_fb"] = (word1_high >> 6) & 1  
-    
-    data["trolleyPos"] = round(float(struct.unpack("i",raw_data[2:6])[0])/1000,3) 
-    data["hoistPos"] = round(float(struct.unpack("i",raw_data[6:10])[0])/1000,3)
-    data["loadPosX"] = round(float(struct.unpack("i",raw_data[10:14])[0])/1000,3)
-    data["trolleySpeed"] = round(float(struct.unpack("h",raw_data[14:16])[0])/16384*4.0,3)
-    data["hoistSpeed"] = round(float(struct.unpack("h",raw_data[16:18])[0])/16384*3.0,3)
-    data["loadSpeedX"] = round(float(struct.unpack("h",raw_data[18:20])[0])/1000,3)
-    data["skewAngle"] = round(float(struct.unpack("h",raw_data[20:22])[0])/1000,3)
-    data["twinGap"] = struct.unpack("h",raw_data[22:24])[0]
-
-    return data
+    print(data_frame.data_to_json())
 
 async def main():
     client = mqtt.Client()
@@ -59,7 +23,7 @@ async def main():
     client.on_connect = on_connect
     client.on_message = on_message
 
-    client.connect("10.0.1.213", 1883, 60)
+    client.connect("localhost", 1883, 60)
 
     client.loop_start()
 
